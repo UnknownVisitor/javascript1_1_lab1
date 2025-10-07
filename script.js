1d860854be223f63c0a8ea9984c4b9090e9376f1
UnknownVisitor <79756736+UnknownVisitor@users.noreply.github.com>
2025-10-07 09:14:16 +0000

    Com1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-(function () {
-  //TODO
+(function() {
+  //ex1_button.
+  const button = document.getElementById('ex1_button');
+  const contentDiv = document.getElementById('ex1_content');
+
+  //Funkcja generująca ciąg liczb od 0 do 9, oddzielonych przecinkami.
+  function generateNumberString() {
+    let result = '';
+    for (let i = 0; i <= 9; i++) {
+      result += i;
+      if (i < 9) {
+        result += ',';
+      }
+    }
+    return result;
+  }
+
+  //Dodaj zdarzenie po przyciśnięciu przycisku.
+  button.addEventListener('click', function() {
+    //Wygeneruj ciąg liczb jako wartość div
+    contentDiv.textContent = generateNumberString();
+  });
 })();
